1211bddab02ba2b5838e58477810028e44a7364b
yong <yong723.enjoy.everything@gmail.com>
2022-08-18 17:37:09 +0900

    add code for creating hparams tuning job

---
+++ vertex_ai/scripts/hparams_tuning/create_job.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+
+region=""
+gcp_project=""
+
+config_file="vertex_ai/configs/hparams_tuning_job/default.yaml"
+git_hash=$(git rev-parse HEAD)
+display_name=$git_hash # you can edit display name whatever you want. defaults is hash value of git
+
+# image_uri is obtained from config_file with the following code
+pip install yaml
+image_uri=$(python << EOF
+import yaml
+
+with open('${config_file}') as f:
+    config = yaml.safe_load(f)
+print(config['trialJobSpec']['workerPoolSpecs']['containerSpec']['imageUri'])
+EOF
+)
+
+# I don't know why but somehow maxTrialCount and parallelTrialCount in config don't recognized by gcloud
+# so that these parameters are obtained from config_file
+
+maxTrialCount=$(python << EOF
+import yaml
+
+with open('${config_file}') as f:
+    config = yaml.safe_load(f)
+print(config['maxTrialCount'])
+EOF
+)
+
+parallelTrialCount=$(python << EOF
+import yaml
+
+with open('${config_file}') as f:
+    config = yaml.safe_load(f)
+print(config['parallelTrialCount'])
+EOF
+)
+
+
+docker build . -t $image_uri  --platform=linux/x86_64 
+docker push $image_uri
+
+# https://cloud.google.com/sdk/gcloud/reference/ai/hp-tuning-jobs/create?hl=ja
+gcloud ai hp-tuning-jobs create \
+    --display-name=$display_name \
+    --region=$region \
+    --project=$gcp_project \
+    --config=$config_file \
+    --max-trial-count=$maxTrialCount \
+    --parallel-trial-count=$parallelTrialCount \
+    --enable-web-access
---
+++ vertex_ai/scripts/hparams_tuning/train_hparams_tuning.py
@@ -0,0 +1,44 @@
+# This file is slightly modified from train.py to send optimized_metric to vertex ai for hyperparameter tuning job
+# can be run from any place
+
+import hydra
+import pyrootutils
+from omegaconf import DictConfig
+import hypertune
+
+# project root setup
+# searches for root indicators in parent dirs, like ".git", "pyproject.toml", etc.
+# sets PROJECT_ROOT environment variable (used in `configs/paths/default.yaml`)
+# loads environment variables from ".env" if exists
+# adds root dir to the PYTHONPATH (so this file can be run from any place)
+# https://github.com/ashleve/pyrootutils
+root = pyrootutils.setup_root(__file__, dotenv=True, pythonpath=True)
+
+
+@hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
+def main(cfg: DictConfig) -> float:
+
+    # imports can be nested inside @hydra.main to optimize tab completion
+    # https://github.com/facebookresearch/hydra/issues/934
+    from src.tasks.train_task import train
+    from src.utils import get_metric_value
+
+    # train the model
+    metric_dict, _ = train(cfg)
+
+    # safely retrieve metric value for hydra-based hyperparameter optimization
+    metric_value = get_metric_value(
+        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+    )
+    
+    hpt = hypertune.HyperTune()
+    hpt.report_hyperparameter_tuning_metric(
+        hyperparameter_metric_tag="metric", metric_value=metric_value
+    )
+
+    # return optimized metric
+    return metric_value
+
+
+if __name__ == "__main__":
+    main()
